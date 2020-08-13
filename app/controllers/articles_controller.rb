d0d1e4e482ca3f67088db5ba6088173de990ff65
Seetaram Yadav <neelamyadav@Seetaram-Yadav-MacBook-Pro.local>
2020-08-13 17:08:13 +0530

    Added the delete behaviour.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,13 @@
 class ArticlesController < ApplicationController
+   
+    def index
+        @articles = Article.all
+    end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+    
     def new
         @article = Article.new
     end
@@ -16,14 +25,6 @@ class ArticlesController < ApplicationController
             render 'new'
         end
     end
-
-    def show
-        @article = Article.find(params[:id])
-    end
-
-    def index
-        @articles = Article.all
-    end
      
     def update
         @article = Article.find(params[:id])
@@ -33,6 +34,13 @@ class ArticlesController < ApplicationController
         else
           render 'edit'
         end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+       
+        redirect_to articles_path
     end
 
     private
